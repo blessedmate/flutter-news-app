59c353049b9d42e7b5d4691ba07fc91879c21141
Mate <md.quintero@uniandes.edu.co>
2021-12-03 11:00:54 -0500

    display top headlines

--- lib/src/widgets/new.dart
+++ lib/src/widgets/new.dart
@@ -19,6 +19,9 @@ class New extends StatelessWidget {
         _HeaderCard(article: article, index: index),
         _TitleCard(article: article),
         _ImageCard(article: article),
+        _BodyCard(article: article),
+        const _ButtonsCard(),
+        const Divider(height: 20),
       ],
     );
   }
@@ -83,7 +86,73 @@ class _ImageCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text('Hello World'),
+      margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(30),
+        child: Container(
+          child: (article.urlToImage != null)
+              ? FadeInImage(
+                  imageErrorBuilder: (context, error, stackTrace) {
+                    return Text(
+                      '   Image not loaded   ',
+                      style: TextStyle(
+                          fontSize: 25, color: appTheme.colorScheme.secondary),
+                    );
+                  },
+                  placeholder: const AssetImage('assets/giphy.gif'),
+                  image: NetworkImage(article.urlToImage!),
+                )
+              : const Image(
+                  image: AssetImage('assets/no-image.png'),
+                ),
+        ),
+      ),
+    );
+  }
+}
+
+class _BodyCard extends StatelessWidget {
+  final Article article;
+  const _BodyCard({
+    Key? key,
+    required this.article,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Text(article.description ?? ''),
+    );
+  }
+}
+
+class _ButtonsCard extends StatelessWidget {
+  const _ButtonsCard({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        RawMaterialButton(
+          onPressed: () {},
+          fillColor: appTheme.colorScheme.secondary,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+          child: const Icon(Icons.star_border),
+        ),
+        const SizedBox(width: 10),
+        RawMaterialButton(
+          onPressed: () {},
+          fillColor: Colors.blue,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+          child: const Icon(Icons.more),
+        ),
+      ],
     );
   }
 }
